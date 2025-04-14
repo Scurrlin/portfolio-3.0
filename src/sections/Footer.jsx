9b44c6e1c3802fb1c850c444f370d900eab0aab7
Sean Currlin <sean.currlin@gmail.com>
2025-04-14 12:21:00 -0400

    update Footer.jsx

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -3,20 +3,20 @@ import { socialImgs } from "../constants";
 const Footer = () => {
   return (
     <footer className="footer">
-      <div className="footer-container">
-        <div className="flex flex-col sm:flex-row w-full items-center justify-between gap-4">
-          <p>Copyright © {new Date().getFullYear()} Sean Currlin</p>
-          <div className="socials flex gap-4">
-            {socialImgs.map((socialImg, index) => (
-              <div key={index} className="icon w-8 h-8">
-                <img 
-                  src={socialImg.imgPath} 
-                  alt="social icon"
-                  className="w-full h-full"
-                />
-              </div>
-            ))}
-          </div>
+      <div className="flex flex-col sm:flex-row w-full items-center justify-between gap-6 sm:gap-4">
+        <p className="pl-4">
+          Copyright © {new Date().getFullYear()} Sean Currlin
+        </p>
+        <div className="socials flex gap-4 pr-4">
+          {socialImgs.map((socialImg, index) => (
+            <div key={index} className="icon w-8 h-8">
+              <img 
+                src={socialImg.imgPath} 
+                alt="social icon"
+                className="w-full h-full"
+              />
+            </div>
+          ))}
         </div>
       </div>
     </footer>
