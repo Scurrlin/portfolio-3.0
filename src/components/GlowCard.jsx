a0a026ba58df8e9feddc93d9438820c784a0d7ca
Sean Currlin <sean.currlin@gmail.com>
2025-04-16 23:39:13 -0400

    company name experiment

--- src/components/GlowCard.jsx
+++ src/components/GlowCard.jsx
@@ -34,7 +34,7 @@ const GlowCard = ({ card, index, children }) => {
     >
       <div className="glow"></div>
       <div className="mb-5 text-center">
-        <h2 className="text-3xl font-semibold inline-block text-transparent bg-clip-text bg-gradient-to-r from-[#007cef] to-[#39d25f]">{card.company}</h2>
+        <h2 className={`text-3xl font-semibold inline-block text-transparent bg-clip-text bg-gradient-to-r from-[${card.gradient?.from || '#fff'}] to-[${card.gradient?.to || '#fff'}]`}>{card.company}</h2>
       </div>
       <div className="mb-3">
         <p className="text-white-50 text-xl font-medium flex items-center justify-center gap-2">
