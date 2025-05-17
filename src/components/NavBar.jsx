28eba9296caaa52b158c6dad8ca6e4c4237e7077
Sean Currlin <sean.currlin@gmail.com>
2025-05-17 13:19:19 -0400

    update resume link

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -43,7 +43,7 @@ const NavBar = () => {
         </nav>
 
         <a 
-          href="https://drive.google.com/file/d/1Wtf5aBz8h6un-LGzzck2NPoBcMU9o2g9/view?usp=sharing" 
+          href="https://drive.google.com/file/d/1CY_gyT3EHxYqb2wKRUirCV-zpvato7Y-/view?usp=sharing" 
           target="_blank" 
           rel="noopener noreferrer" 
           className="contact-btn group"
